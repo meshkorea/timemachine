6e15a191113a891adabec7b2f6af00bc26a87c59
hymesh <hoyong.jung@meshkorea.net>
2019-09-06 19:38:07 +0900

    PP-720 Allowed Env 대신 Timemachine Enabled 설정 사용하도록 변경

---
+++ src/middlewares/CheckIfTimemachineEnabled.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Appkr\Timemachine;
+
+use Closure;
+use Illuminate\Support\Facades\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class CheckIfTimemachineEnabled
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request $request
+     * @param  Closure $next
+     *
+     * @throws UnauthorizedHttpException
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $enabled = config('timemachine.enabled');
+
+        if (! $enabled) {
+            throw new NotFoundHttpException();
+        }
+
+        return $next($request);
+    }
+}
